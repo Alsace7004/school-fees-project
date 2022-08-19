229a0ac81948c669cbfe3b99d6c71949d5345efd
Alsace7004 <alabibamidele14@gmail.com>
2022-08-19 16:00:45 +0200

    add text/css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,23 +8,23 @@
         <!-- Google Font: Source Sans Pro -->
         <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">-->
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/fontawesome-free/css/all.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/fontawesome-free/css/all.min.css")}}" type="text/css">
         <!-- Ionicons -->
         <!--<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">-->
         <!-- Tempusdominus Bootstrap 4 -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css")}}" type="text/css">
         <!-- iCheck -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css")}}" type="text/css">
         <!-- JQVMap -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/jqvmap/jqvmap.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/jqvmap/jqvmap.min.css")}}" type="text/css">
         <!-- Theme style -->
-        <link rel="stylesheet" href="{{asset("admin/dist/css/adminlte.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/dist/css/adminlte.min.css")}}" type="text/css">
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}" type="text/css">
         <!-- Daterange picker -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/daterangepicker/daterangepicker.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/daterangepicker/daterangepicker.css")}}" type="text/css">
         <!-- summernote -->
-        <link rel="stylesheet" href="{{asset("admin/plugins/summernote/summernote-bs4.min.css")}}">
+        <link rel="stylesheet" href="{{asset("admin/plugins/summernote/summernote-bs4.min.css")}}" type="text/css">
 
         
     </head>
